05166eb01d027dc327af0adf43ff0f23b6c03df7
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-05-13 12:05:04 +0300

    feat(source): migrate to customize-cra

---
+++ src/Instagram/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/init.css';
+
+export const Instagram = () => {
+    return (
+        <section className = 'welcome'>
+            <h1>Добро пожаловать!</h1>
+        </section>
+    );
+};
