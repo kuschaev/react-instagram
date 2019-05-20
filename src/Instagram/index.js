77b1ee9355f37830ffc0f5d6a53e503d45119490
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-05-20 19:15:51 +0300

    → Checkpoint at: 20:43:41

--- src/Instagram/index.js
+++ src/Instagram/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles/init.css';
 
-import { Header } from '../components/Header';
-import { Content } from '../components/Content';
+import { Header } from './components/Header';
+import { Content } from './components/Content';
 
 export const Instagram = () => {
     return (
