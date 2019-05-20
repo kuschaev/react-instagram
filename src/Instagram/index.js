4bf09a8b92fd0f87db939616f0668b8fc5004f7c
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-05-20 19:15:51 +0300

    → Checkpoint at: 20:26:57

--- src/Instagram/index.js
+++ src/Instagram/index.js
@@ -3,17 +3,20 @@ import './styles/init.css';
 
 export const Instagram = () => {
     return (
-        <section className = 'header'>
-            <div className = 'logo' />
-            <input
-                placeholder = 'Search'
-                type = 'text'
-            />
-            <div className = 'nav'>
-                <div className = 'explore' />
-                <div className = 'likes' />
-                <div className = 'settings' />
-            </div>
-        </section>
+        <>
+            <section className = 'header'>
+                <div className = 'logo' />
+                <input
+                    placeholder = 'Search'
+                    type = 'text'
+                />
+                <div className = 'nav'>
+                    <div className = 'explore' />
+                    <div className = 'likes' />
+                    <div className = 'settings' />
+                </div>
+            </section>
+            <section className = 'content' />
+        </>
     );
 };
