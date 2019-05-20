d542963306b13a261d47f9957228034079710ca4
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-05-20 19:15:51 +0300

    → Checkpoint at: 20:42:03

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import avatar from './assets/images/nasa.jpg';
 
-export const Header = () => {
+export const Content = () => {
     return (
-        <section className = 'header'>
-            <div className = 'logo' />
-            <input
-                placeholder = 'Search'
-                type = 'text'
-            />
-            <div className = 'nav'>
-                <div className = 'explore' />
-                <div className = 'likes' />
-                <div className = 'settings' />
+        <section className = 'content'>
+            <div className = 'profile'>
+                <img
+                    className = 'avatar'
+                    src = { avatar }
+                />
+                <div className = 'main'>
+                    <span className = 'nickname'>nasa</span>
+                    <div className = 'stats'>
+                        <div>
+                            <span>2.614</span>&nbsp;posts
+                        </div>
+                        <div>
+                            <span>42.4m</span>&nbsp;followers
+                        </div>
+                        <div>
+                            <span>84</span>&nbsp;following
+                        </div>
+                    </div>
+                    <span className = 'name'>NASA</span>
+                </div>
             </div>
         </section>
     );
